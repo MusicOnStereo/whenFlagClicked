e0d8bfcf6cf273d74541f467f01dec4779906c15
musiconmono <80660200+MusicOnStereo@users.noreply.github.com>
2022-01-19 19:04:16 +0000

    Initial commit

---
+++ whenFlagClicked/components/NavBar.vue
@@ -0,0 +1,39 @@
+<template>
+	<div style="overflow: auto">
+		<div id="navbar">
+			<p style="font-size: 20px"><b>
+				<ul>
+					<li><img src="~/assets/TempLogo.svg" width=50 height=50/></li>
+					<li>Home</li>
+					<li>Tutorials</li>
+					<li>About</li>
+				</ul>
+			</b></p>
+		</div>
+		<br>
+	</div>
+</template>
+
+<style>
+	#navbar {
+		overflow-x: hidden;
+	}
+	#navbar ul {
+		list-style-type: none;
+		margin: 0;
+		padding: 0;
+		width: 80%;
+		align-items: stretch;
+		display: flex;
+		justify-content: space-between;
+		position: relative;
+		left: 50%;
+	}
+	#navbar ul li {
+		position: relative;
+		left: -50%;
+		text-align: center;
+		display: block;
+		flex: 0 0 auto;
+	}
+</style>
---
+++ whenFlagClicked/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		<NavBar />
+		<p>test</p>
+	</div>
+</template>
+
+<style>
+	body {
+		background-image: url(~assets/MainBackground.png);
+		background-repeat: repeat;
+	}
+</style> 
